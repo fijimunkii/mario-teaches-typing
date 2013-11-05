c88729c977da79bc3590607bfe30e98f15e5ca63
Harrison Powers <fijimunkii@gmail.com>
2013-11-05 16:10:50 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Typestar::Application.config.secret_token = '90c60d064988ca2af519f8277de22ba39928ade5c9659886f4aeeaf3cec47158584a76b31b60b499f31b38f5907babbee0293ea5c7f8acfab7774949d27ee3ac'
